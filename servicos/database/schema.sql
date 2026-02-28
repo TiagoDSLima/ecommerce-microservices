5c3237d2e9364abc564e1be5e8c0676fd3d0bb71
TiagoDSLima <tiagodslima51@gmail.com>
2026-02-28 15:04:33 -0300

    config(Auth): Criando conexão com banco de dados

--- servicos/database/schema.sql
+++ servicos/database/schema.sql
@@ -5,6 +5,6 @@ CREATE TABLE usuario(
     id SERIAL NOT NULL PRIMARY KEY,
     nome VARCHAR(150) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL
 );
 ----------------------

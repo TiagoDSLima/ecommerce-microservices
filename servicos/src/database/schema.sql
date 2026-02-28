0e12edd85da6bc08426578889a3a8d55dc9710bf
TiagoDSLima <tiagodslima51@gmail.com>
2026-02-28 14:50:23 -0300

    config(servicos): Criando serviço para conteiners e frameworks

---
+++ servicos/src/database/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE ecommerceauth;
+
+-------- AUTH --------
+CREATE TABLE usuario(
+    id SERIAL NOT NULL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+);
+----------------------

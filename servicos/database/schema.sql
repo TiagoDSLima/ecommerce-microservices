9e65de8aa6b73da1f8125cba2f0aaa5fa2cb59de
tiagodslima51@gmail.com <tiagodslima51@gmail.com>
2026-03-23 09:28:14 -0300

    refactor(Servicos): ajustando código SQL

--- servicos/database/schema.sql
+++ servicos/database/schema.sql
@@ -1,17 +1,18 @@
-CREATE DATABASE ecommerceauth;
-CREATE DATABASE ecommerceparticipante;
-
 -------- AUTH --------
+
+CREATE DATABASE ecommerceauth;
 
 CREATE TABLE usuario(
     id SERIAL NOT NULL PRIMARY KEY,
     email VARCHAR(150) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
-    role VARCHAR(15) NOT NULL DEFAULT 'ROLE_USER' CHECK (role IN ('ROLE_USER','ROLE_ADMIN')
+    role VARCHAR(15) NOT NULL DEFAULT 'ROLE_USER' CHECK (role IN ('ROLE_USER','ROLE_ADMIN'))
 );
 ----------------------
 
 -------- PARTICIPANTE --------
+
+CREATE DATABASE ecommerceparticipante;
 
 CREATE TABLE participante(
     id SERIAL NOT NULL PRIMARY KEY,

25f8a4dd5d775df297fcb1e67a04b28e4b3450f0
tiagodslima51@gmail.com <tiagodslima51@gmail.com>
2026-03-24 09:24:41 -0300

    config(Produto/Servicos): Adicionando suporte ao MINIO

---
+++ produto/src/main/java/com/br/ecommerce/produto/service/BucketService.java
@@ -0,0 +1,55 @@
+package com.br.ecommerce.produto.service;
+
+import com.br.ecommerce.produto.config.props.MinioProps;
+import com.br.ecommerce.produto.dto.BucketFile;
+import io.minio.GetPresignedObjectUrlArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.http.Method;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class BucketService {
+
+    private final MinioClient minioClient;
+    private final MinioProps minioProps;
+
+    public void upload(BucketFile bucketFile) {
+        try{
+            var object = PutObjectArgs
+                    .builder()
+                    .bucket(minioProps.getBucketName())
+                    .object(bucketFile.name())
+                    .stream(bucketFile.is(), bucketFile.size(), -1)
+                    .contentType(bucketFile.type().toString())
+                    .build();
+            minioClient.putObject(object);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getUrl(Long idProduto){
+        try{
+            var obejct = GetPresignedObjectUrlArgs
+                    .builder()
+                    .method(Method.GET)
+                    .bucket(minioProps.getBucketName())
+                    .object(retornaNomeProduto(idProduto))
+                    .expiry(1, TimeUnit.DAYS)
+                    .build();
+
+            return minioClient.getPresignedObjectUrl(obejct);
+        } catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String retornaNomeProduto(Long idProduto) {
+        return String.format("produto-%d.pdf", idProduto);
+    }
+}

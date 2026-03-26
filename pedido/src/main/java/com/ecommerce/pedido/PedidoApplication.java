a757d7f79eeeff3de95caead6b5c431857bdac4f
tiagodslima51@gmail.com <tiagodslima51@gmail.com>
2026-03-26 15:25:53 -0300

    refactor(Pedido): Trocando o projeto Pedido que utilizava Gradle por um Maven

--- pedido/src/main/java/com/ecommerce/pedido/PedidoApplication.java
+++ pedido/src/main/java/com/ecommerce/pedido/PedidoApplication.java
@@ -1,4 +1,4 @@
-package com.br.ecommerce.pedido;
+package com.ecommerce.pedido;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- pedido/src/test/java/com/ecommerce/pedido/PedidoApplicationTests.java
+++ pedido/src/test/java/com/ecommerce/pedido/PedidoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.br.ecommerce.pedido;
+package com.ecommerce.pedido;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

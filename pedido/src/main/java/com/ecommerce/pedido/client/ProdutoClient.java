c874ffc73587f333ebd58d5e070c94a50c37a32b
guilhermeguidat <guilhermeguida01@gmail.com>
2026-04-13 21:22:58 -0300

    feat: valida produto antes de criar pedido

--- pedido/src/main/java/com/ecommerce/pedido/client/ProdutoClient.java
+++ pedido/src/main/java/com/ecommerce/pedido/client/ProdutoClient.java
@@ -1,7 +1,13 @@
 package com.ecommerce.pedido.client;
 
+import com.ecommerce.pedido.dto.ProdutoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "produto", url = "${ecommerce.config.clients.produto.url}")
 public interface ProdutoClient {
+
+    @GetMapping(value = "/busca/{idProduto}")
+    ProdutoResponse buscar(@PathVariable("idProduto") Long idProduto);
 }

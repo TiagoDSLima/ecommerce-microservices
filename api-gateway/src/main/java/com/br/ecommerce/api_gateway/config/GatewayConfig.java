5db473850fcd2f8a59f70b2bb1d350aeb261185e
tiagodslima51@gmail.com <tiagodslima51@gmail.com>
2026-04-27 10:19:03 -0300

    feat(Gateway): Colocando rotas e retirando verificação de token no Gateway

---
+++ api-gateway/src/main/java/com/br/ecommerce/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,31 @@
+package com.br.ecommerce.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r
+                        .path("/auth/**")
+                        .uri("http://localhost:8081"))
+                .route("participante-service", r -> r
+                        .path("/participante/**")
+                        .uri("http://localhost:8082"))
+                .route("produto-service", r -> r
+                        .path("/produto/**")
+                        .uri("http://localhost:8083"))
+                .route("loja-service", r -> r
+                        .path("/loja/**")
+                        .uri("http://localhost:8084"))
+                .route("pedido-service", r -> r
+                        .path("/pedido/**")
+                        .uri("http://localhost:8085"))
+                .build();
+    }
+}
